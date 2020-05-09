a50b63abc42ca3b261d107b0a7c06289204c2044
vijayvir <virvijay37@gmail.com>
2020-05-09 15:24:32 +0530

    v

---
+++ LeoVip/LeoVip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LeoVip
+//
+//  Created by tecH on 09/05/20.
+//  Copyright © 2020 vijayvir Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
