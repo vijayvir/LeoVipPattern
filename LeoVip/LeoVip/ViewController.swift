652d2be55cfbceb0fee6df4b9d6a0b53c2752a75
vijayvir <virvijay37@gmail.com>
2020-05-11 10:37:23 +0530

    d
    d

--- LeoVip/LeoVip/ViewController.swift
+++ LeoVip/LeoVip/ViewController.swift
@@ -7,9 +7,43 @@
 //
 
 import UIKit
+class Presenter {
+var contoller : Controller?
+    
+}
+class Interator {
+    var firstWorker = FirstWorker()
+}
+
+class FirstWorker {
+    let model : Model = Model()
+}
+class SecondWorker {
+    
+    
+}
+
+
+class Model {
+    
+}
+// https://medium.com/@saad.eloulladi/clean-architecture-for-ios-viper-vip-c19de40b71e2
+class Controller {
+    
+    var interactor: Interator? = Interator()
+    var view = View()
+}
+
+class View {
+    
+}
+
+class Router {
+     var contoller : Controller?
+}
 
 class ViewController: UIViewController {
-
+    var controller =  Controller()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
---
+++ LeoVip/LeoVip/sample2.swift
@@ -0,0 +1,9 @@
+//
+//  sample2.swift
+//  LeoVip
+//
+//  Created by tecH on 10/05/20.
+//  Copyright © 2020 vijayvir Singh. All rights reserved.
+//
+
+import Foundation
